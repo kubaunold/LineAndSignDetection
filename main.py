1ce10e0869eb77fa37db8b2cdb870d94497ecb80
Your Name <jakub@unold.pl>
2022-01-27 12:12:51 +0100

    added new objective: focus only on line detection

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import line_detection as ld
-import sign_detection as sd
+# import sign_detection as sd
 
 """
 PROGRAM ŁĄCZĄCY WYKRYWANIE ZNAKÓW I LINII NA OBRAZIE
 (w czasie rzeczywistym)
+
+[UPDATE 27.01.2022]
+robimy tylko jazde pomiedzy liniami; poki co zostawiamy wykrywaniem
+znakow, bo jest problem z zainstalowaniem tensorflow'a
 """
 
 def main():
